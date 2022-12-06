d737ddd5368c0d9cd75904599519238106b60fb7
esau-morais <esaumorais7@gmail.com>
2022-12-06 11:12:32 -0300

    feat(analytics): add web vitals report

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -2,6 +2,8 @@ import { startTransition, StrictMode } from 'react'
 import { hydrateRoot } from 'react-dom/client'
 
 import { RemixBrowser } from '@remix-run/react'
+import reportWebVitals from './reportWebVitals'
+import { sendToVercelAnalytics } from './vitals'
 
 const hydrate = () => {
   startTransition(() => {
@@ -21,3 +23,5 @@ if (window.requestIdleCallback) {
   // https://caniuse.com/requestidlecallback
   window.setTimeout(hydrate, 1)
 }
+
+reportWebVitals(sendToVercelAnalytics)
