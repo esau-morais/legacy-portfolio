4d25d3e937c1b0c0d2f14ee79d1410a268226732
esau-morais <esaumorais7@gmail.com>
2022-12-08 18:08:23 -0300

    feat(webvitals): add metronome

--- server.js
+++ server.js
@@ -1,4 +1,20 @@
-import { createRequestHandler } from "@remix-run/vercel";
-import * as build from "@remix-run/dev/server-build";
+import * as build from '@remix-run/dev/server-build'
+import { createRequestHandler } from '@remix-run/vercel'
 
-export default createRequestHandler({ build, mode: process.env.NODE_ENV });
+import {
+  createMetronomeGetLoadContext,
+  registerMetronome,
+} from '@metronome-sh/vercel'
+
+const buildWithMetronome = registerMetronome(build)
+
+const metronomeGetLoadContext = createMetronomeGetLoadContext(
+  buildWithMetronome,
+  { config: require('./metronome.config.js') }
+)
+
+export default createRequestHandler({
+  build: buildWithMetronome,
+  getLoadContext: metronomeGetLoadContext,
+  mode: process.env.NODE_ENV
+})
